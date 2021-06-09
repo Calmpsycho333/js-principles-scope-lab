955602e41208258a956958f6970f25ee4e328827
calmpsycho333 <wrathking75@yahoo.com>
2021-06-09 02:50:13 -0400

    commit tree 843d33c2f9a28a830f6ec74346408687a6f94f47

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+var customerName = `bob`;
+
+
+
+function upperCaseCustomerName(customerName) {
+    return customerName.toUpperCase;
+};
+
+
+
+function setBestCustomer() {
+    bestCustomer = `not bob`;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function upperCaseCustomerName() {
+    customerName = `BOB`;
+}
+
+const leastFavoriteCustomer = 2
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 1;
+};
